921c7a91082034cbc9ed84dfe4fa67ac07217088
NoYoWiFi <1284524409@qq.com>
2024-04-07 17:25:44 +0800

    [2024-4-7 17:04:43] 6.4.13

--- update_config-entrypoint_pgsql.sh
+++ update_config-entrypoint_pgsql.sh
@@ -144,7 +144,7 @@ elif [ $# -ge 1 ]; then
         exit 1
     fi
 
-    if [[ "$1" == "cp_proxy_pgsql" ]]; then
+    if [[ "$1" == "cp_proxy" ]]; then
         option=$(echo ${GV_VERSION} | cut -c 1)
         case ${option} in
             5)
