a593f550c746040ba3ae142c6843fbfd91ddb42f
NoYoWiFi <1284524409@qq.com>
2026-06-10 11:52:33 +0800

    [2026-6-10 11:49:08]
    更新：更新README.md

--- autosetup.sh
+++ autosetup.sh
@@ -183,6 +183,7 @@ chmod a+x /usr/bin/zabbix_trap_receiver.pl
 sed -i "/# authCommunity   log,execute,net public/s/# authCommunity   log,execute,net public/authCommunity   log,execute,net public/" /etc/snmp/snmptrapd.conf
 sed -i "/zabbix_trap_receiver.pl/d" /etc/snmp/snmptrapd.conf
 echo "perl do \"/usr/bin/zabbix_trap_receiver.pl\"" >> /etc/snmp/snmptrapd.conf
+\cp ./snmptrap/snmptrapd.conf /etc/snmp/
 #![创建SNMP V3用户]
 sed -i -e "/rouser/d" /etc/snmp/snmpd.conf
 sed -i -e "/zabbix/d" /var/lib/net-snmp/snmpd.conf
@@ -248,7 +249,7 @@ sed -i "/# ListenIP=0.0.0.0/s/# ListenIP=0.0.0.0/ListenIP=0.0.0.0/" ${ZABBIX_CON
 sed -i "/# JavaGateway=/s/# JavaGateway=/JavaGateway=127.0.0.1/" ${ZABBIX_CONFIG}
 sed -i "/# JavaGatewayPort=10052/s/# JavaGatewayPort=10052/JavaGatewayPort=10052/" ${ZABBIX_CONFIG}
 sed -i "/# StartJavaPollers=0/s/# StartJavaPollers=0/StartJavaPollers=5/" ${ZABBIX_CONFIG}
-sed -i "/# SNMPTrapperFile=\/tmp\/zabbix_traps.tmp/s/# SNMPTrapperFile=\/tmp\/zabbix_traps.tmp/SNMPTrapperFile=\/tmp\/zabbix_traps.tmp/" ${ZABBIX_CONFIG}
+sed -i "/# SNMPTrapperFile=\/tmp\/zabbix_traps.tmp/s/# SNMPTrapperFile=\/tmp\/zabbix_traps.tmp/SNMPTrapperFile=\/var\/log\/loki\/zabbix_traps.log/" ${ZABBIX_CONFIG}
 sed -i "/^SNMPTrapperFile=\/var\/log\/snmptrap\/snmptrap.log/s/SNMPTrapperFile=\/var\/log\/snmptrap\/snmptrap.log/# SNMPTrapperFile=\/var\/log\/snmptrap\/snmptrap.log/" ${ZABBIX_CONFIG}
 sed -i "/# AllowUnsupportedDBVersions=0/s/# AllowUnsupportedDBVersions=0/AllowUnsupportedDBVersions=1/" ${ZABBIX_CONFIG}
 sed -i -e "/^\# DebugLevel/s/=.*/=1/" ${ZABBIX_CONFIG}
